fb71737080428b58d22c23a3437ea9088f08cce8
kztera <116504460+kztera@users.noreply.github.com>
2024-05-16 13:53:50 +0700

    feat: Fix bug preventing adding duplicate products to cart

--- controllers/carts.js
+++ controllers/carts.js
@@ -50,12 +50,12 @@ exports.addToCart = async function (req, res) {
       return res.status(404).json({ message: 'Product not found' });
     }
 
-    const existingCartItem = user.cart.find(
-      (item) => item.product.toString() === productId
-    );
-    if (existingCartItem) {
-      await session.abortTransaction();
-      return res.status(400).json({ message: 'Product already in cart' });
+    if (user.cart.length) {
+      const existingCartItem = user.cart.find((item) => item.equals(productId));
+      if (existingCartItem) {
+        await session.abortTransaction();
+        return res.status(400).json({ message: 'Product already in cart' });
+      }
     }
 
     const cart = await new Cart({
